b4efa691874007af8d2201256e7fdb8728b73cce
宗宸·谢尔比 <88515720+ChinaRan0@users.noreply.github.com>
2025-01-06 11:30:06 +0800

    Add files via upload

---
+++ rule/JavaChains.py
@@ -0,0 +1,10 @@
+import requests
+def check(ip,port):
+    try:
+        res =requests.get(f"http://{ip}:{port}/#/login",verify=False,timeout=3)
+        res.encoding="utf-8"
+        # print(res.text)
+        if "Java Chains" in res.text:
+            print(f"http://{ip}:{port}/----Java Chains(Java反序列化攻击工具)")
+    except:
+        pass
