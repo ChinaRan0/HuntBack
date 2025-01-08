f2f3ebcc743c9a4d2940880574f9a168ca239d9b
宗宸·谢尔比 <88515720+ChinaRan0@users.noreply.github.com>
2025-01-08 11:15:51 +0800

    Add files via upload

---
+++ rule/afrog.py
@@ -0,0 +1,11 @@
+import requests
+def check(ip,port):
+    try:
+        res =requests.get(f"http://{ip}:{port}/",verify=False,timeout=3)
+        res.encoding="utf-8"
+        # print(res.text)
+        if "afrog" in res.text:
+
+            print(f"http://{ip}:{port}/----afrog")
+    except:
+        pass
